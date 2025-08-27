b9f7e882f537e0c58f39a48c4b6e12a4106c1d97
mhd <mahmoud.shuman@outlook.com>
2025-08-27 08:58:04 +0300

    django

---
+++ django/django_fundamentals/assignments/first_django_project/the_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+# Create your views here.
+
+def root(request):
+    return redirect(index)
+
+def index(request):
+    return HttpResponse("plaseholder to display a list blogs")
+
+def new(request):
+    return HttpResponse("plaseholder to display a new form to create a new blog")
+
+def creat(request):
+    return redirect(root)
+
+def show(request, num):
+    return HttpResponse(f"placeholder to display blog number:{num}")
+
+def edit(request, num):
+    return HttpResponse(f"placeholder to edit blog:{num}")
+
+def destroy(request):
+    return redirect(index)
+
+def bonus(request):
+    return JsonResponse({
+        'title':'Mahmoud J Shuman',
+        'content':'Axsos assingmets'
+    })
