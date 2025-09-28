0177b71fff3916dc3aa322f07e5e3a9b917fa9f9
mhd <mahmoud.shuman@outlook.com>
2025-09-28 16:26:44 +0300

    orm srtvs

---
+++ django/django_orm/assignments/semi_restfull_tv_show/srtvs_app/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from . import models
+from .models import Show
+# Create your views here.
+def root(request):
+    return redirect("/shows/")
+def index(request):
+    context = {
+        "tv_shows": Show.objects.all()
+    }
+    return render(request, "index.html", context)
+def new(request):
+    return render(request,"new_show.html" )
+def add_show(request):
+        if request.method == 'POST':
+            form_show_id = request.POST['show_id']
+            form_title = request.POST['title']
+            form_network = request.POST['network']
+            form_r_date = request.POST['r_date']
+            form_desc = request.POST['desc']
+            Show.objects.create(title = form_title, network = form_network, release_date = form_r_date,desc = form_desc)
+            return redirect("/shows/"+str(form_show_id))
+
+def show(request, show_id):
+    context = {
+        "the_show": Show.objects.get(id = show_id)
+    }
+    return render(request, "show.html", context)
+
+def delete(request,show_id):
+    to_del = Show.objects.get(id = show_id)
+    to_del.delete()
+    return redirect("/shows/")
+
+def edit_page(request, show_id):
+    context = {
+        "the_show": Show.objects.get(id = show_id)
+    }
+    return render(request, "edit.html", context)
+def edit_method(request):
+    if request.method == 'POST':
+            form_show_id = request.POST['show_id']
+            form_title = request.POST['title']
+            form_network = request.POST['network']
+            form_r_date = request.POST['r_date']
+            form_desc = request.POST['desc']
+            to_update = Show.objects.get(id = form_show_id)
+            to_update.title = form_title
+            to_update.network = form_network
+            to_update.release_date = form_r_date
+            to_update.desc = form_desc
+            to_update.save()
+            return redirect("/shows/"+str(form_show_id))
+
